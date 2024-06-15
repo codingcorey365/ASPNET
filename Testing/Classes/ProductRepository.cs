e7227fcdb451687fff6fa38288b3a1c9a85215fa
Corey Schroeder <codingcorey365@gmail.com>
2024-06-15 08:47:18 -0500

    exercise 4 completed

--- Testing/Classes/ProductRepository.cs
+++ Testing/Classes/ProductRepository.cs
@@ -13,6 +13,13 @@ public class ProductRepository : IProductRepository
     {
         _conn = conn;
     }
+
+    public void InsertProduct(Product productToInsert)
+    {
+        _conn.Execute("INSERT INTO products (NAME, PRICE, CATEGORYID) VALUES (@name, @price, @categoryID);",
+            new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+    }
+
     public IEnumerable<Product> GetAllProducts()
     {
         return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
@@ -22,9 +29,23 @@ public class ProductRepository : IProductRepository
     {
         return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
     }
+
     public void UpdateProduct(Product product)
     {
         _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
             new { name = product.Name, price = product.Price, id = product.ProductID });
+    }
+
+    public IEnumerable<Category> GetCategories()
+    {
+        return _conn.Query<Category>("SELECT * FROM categories;");
+    }
+
+    public Product AssignCategory()
+    {
+        var categoryList = GetCategories();
+        var product = new Product();
+        product.Categories = categoryList;
+        return product;
     }
 }
--- Testing/Repositories/IProductRepository.cs
+++ Testing/Repositories/IProductRepository.cs
@@ -8,4 +8,9 @@ public interface IProductRepository
     public IEnumerable<Product> GetAllProducts();
     public Product GetProduct(int id);
     void UpdateProduct(Product product);
+
+    public void InsertProduct(Product productToInsert);
+    public IEnumerable<Category> GetCategories();
+    public Product AssignCategory();
+
 }
